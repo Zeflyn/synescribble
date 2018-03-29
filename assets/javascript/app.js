8114f7b1eba95cea25f3f0e42fcc1155aac9474a
Aarick-F <zeflyndesign@gmail.com>
2018-03-29 18:58:23 -0400

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+$(document).ready(() => {
+  // Firebase Config
+  var config = {
+    apiKey: "AIzaSyBIxDWLxlf5c7pARWt5wL1LhhiODNA3LMY",
+    authDomain: "synescribble.firebaseapp.com",
+    databaseURL: "https://synescribble.firebaseio.com",
+    projectId: "synescribble",
+    storageBucket: "",
+    messagingSenderId: "648660572319"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+  console.log(database);
+});
