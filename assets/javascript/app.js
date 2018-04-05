75b19b656d59a2e6544cd7d38dfde4991bfb541b
Lisa Huynh <lisaahuynh@yahoo.com>
2018-04-05 18:09:16 -0400

    separated color api ajax call to a separate js file to fix err

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -78,32 +78,32 @@ $(document).ready(() => {
    project.clear();
  });
 
- $('#colorBtn').on("click", function() {
-  // mode choices: monochrome monochrome-dark monochrome-light analogic complement analogic-complement triad quad
-  // analogic-complement mode gives colors that are adjacent and across from each other on the color wheel
-  var hex = "0047AB";
-  var queryURL = "https://cors-anywhere.herokuapp.com/https:\/\/thecolorapi.com/scheme?" + hex + "&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=json&mode=analogic-complement&count=6" 
+//  $('#colorBtn').on("click", function() {
+//   // mode choices: monochrome monochrome-dark monochrome-light analogic complement analogic-complement triad quad
+//   // analogic-complement mode gives colors that are adjacent and across from each other on the color wheel
+//   var hex = "0047AB";
+//   var queryURL = "https://crossorigin.me/https://thecolorapi.com/scheme?" + hex + "&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=json&mode=analogic-complement&count=6" 
 
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).done(function(response) {
-    console.log(response);
+//   $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   }).done(function(response) {
+//     console.log(response);
 
-var colorApi = response.colors;
+// var colorApi = response.colors;
 
-for (i = 0; i < colorApi.length; i++){
-var colorSq = $('<div>').addClass('colorSq');
-var hex = colorApi.hex.value;
-console.log(hex);
+// for (i = 0; i < colorApi.length; i++){
+// var colorSq = $('<div>').addClass('colorSq');
+// var hex = colorApi.hex.value;
+// console.log(hex);
 
-$('#colorPallette').append(colorApi);
-console.log(img);
+// $('#colorPallette').append(colorSq);
+// console.log(img);
 
-        }
-    });
+//         }
+//     });
 
-});
+// });
 
  // Example button that scales the image of the gallery images down
  $("#scale").on("click", function() {
---
+++ assets/javascript/colorapi.js
@@ -0,0 +1,32 @@
+$(document).ready(() => {
+
+    $('#colorBtn').on("click", function() {
+        // mode choices: monochrome monochrome-dark monochrome-light analogic complement analogic-complement triad quad
+        // analogic-complement mode gives colors that are adjacent and across from each other on the color wheel
+        var hex = "0047AB";
+        var queryURL = "https://crossorigin.me/https://thecolorapi.com/scheme?" + hex + "&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=json&mode=analogic-complement&count=6" 
+      
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).done(function(response) {
+          console.log(response);
+      
+      var colorApi = response.colors;
+      
+      for (i = 0; i < colorApi.length; i++){
+      var colorSq = $('<div>').addClass('colorSq');
+      var hex = colorApi.hex.value;
+      console.log(hex);
+      
+      $('#colorPallette').append(colorSq);
+      console.log(img);
+      
+              }
+          });
+      
+      });
+
+
+
+});
